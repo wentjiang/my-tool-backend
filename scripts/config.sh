6d00a658dc356b9bff88f25e4c7829a1dc629b4d
wentjiang <wentjiang@sina.com>
2023-12-20 09:37:58 +0800

    update scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,9 @@
 set -eufo pipefail
 
 REMOTE_SERVER=172.245.156.103
-DOCKER_IMAGE_NAME=wentjiang/my-tool-backend
+
+CONTAINER_NAME=my-tool-backend
+DOCKER_IMAGE_NAME=wentjiang/${CONTAINER_NAME}
 
 export REMOTE_SERVER
 export DOCKER_IMAGE_NAME
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,11 @@ cd "$(dirname "$0")/.."
 
 echo "部署到远程服务器"
 
-ssh root@"${REMOTE_SERVER}" "docker stop ${DOCKER_IMAGE_NAME} || true"
-ssh root@"${REMOTE_SERVER}" "docker rm ${DOCKER_IMAGE_NAME} || true"
+echo "停止docker容器"
+ssh root@"${REMOTE_SERVER}" "docker stop ${CONTAINER_NAME} || true"
+echo "拉取最新的镜像"
 ssh root@"${REMOTE_SERVER}" "docker pull ${DOCKER_IMAGE_NAME}:latest"
-ssh root@"${REMOTE_SERVER}" "docker run -d --name my-tool-backend -p 5001:5001 ${DOCKER_IMAGE_NAME}:latest"
+echo "启动新版本的镜像"
+ssh root@"${REMOTE_SERVER}" "docker run -d --rm --name my-tool-backend -p 5001:5001 ${DOCKER_IMAGE_NAME}:latest"
 
 echo "部署完成!"
