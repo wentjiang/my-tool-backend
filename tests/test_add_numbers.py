4d78b6872557baaf94451ff2ead30fe5b7002158
wentjiang <wentjiang@sina.com>
2023-12-23 20:08:53 +0800

    fix test

--- tests/test_add_numbers.py
+++ tests/test_add_numbers.py
@@ -2,12 +2,12 @@ import unittest
 from datetime import datetime as dt
 import datetime
 
-import application
+from python import tools
 
 
 class TestAddNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
         start_time = dt(2023, 1, 1, 12, 0, 0)
         end_time = dt(2023, 1, 1, 13, 30, 0)
-        result = application.calculate_time_diff(start_time, end_time)
+        result = tools.calculate_time_diff(start_time, end_time)
         self.assertEqual(result, datetime.timedelta(seconds=5400))
